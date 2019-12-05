ef184bbab286b3fec8990f5d0311f6b2ed4bf9bf
KUBASIK <tomkubasik74200@gmail.com>
2019-12-06 00:16:14 +0100

    cko

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-lol
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+	<div id="lieu">	
+		<h3> CAF - Caisse d'allocations familiales </h3>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2792.999759679692!2d5.910104751499735!3d45.57044343426554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478ba850b7306a4f%3A0xea3dbedf096ef341!2sCAF%20-%20Caisse%20d&#39;allocations%20familiales!5e0!3m2!1sfr!2sfr!4v1575576341421!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+	</div>
+	<div id="question_forum">
+		<p>Poser une question</p>
+		<div>
+			<form>
+				<input type="text" name="question" placeholder="Votre question..." id="question_caf"></input>
+			</form>
+	</div>
+
+
+</body>
+</html>
