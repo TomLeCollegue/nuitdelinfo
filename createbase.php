ce6080ef4dc21f8c512775000de4be5b8d31849b
Laurent Cutting <laurent.cutting@gmail.fr>
2019-12-06 01:15:43 +0100

    creer table reponses

--- createbase.php
+++ createbase.php
@@ -8,3 +8,12 @@ $bdd->query("CREATE TABLE IF NOT EXISTS `questions` (
     `valid` boolean,
     PRIMARY KEY (`id_question`)
   );");
+
+$bdd->query("CREATE TABLE IF NOT EXISTS `reponses` (
+    `id_reponse` int NOT NULL AUTO_INCREMENT,
+    `texte` varchar(400) NOT NULL,
+    `date` Date NOT NULL,
+    `id_question` int,
+    PRIMARY KEY (`id_reponse`),
+    FOREIGN KEY (`id_question`) REFERENCES questions(`id_question`)
+  );");
