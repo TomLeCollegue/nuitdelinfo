223b0837a3d5ed4ae9c90d213481d9a166004c79
Laurent Cutting <laurent.cutting@gmail.fr>
2019-12-06 01:08:01 +0100

    creer bd

--- createbase.php
+++ createbase.php
@@ -1,2 +1,10 @@
 <?php
 $bdd  = get_pdo();
+$bdd->query("CREATE TABLE IF NOT EXISTS `questions` (
+    `id_question` int NOT NULL AUTO_INCREMENT,
+    `texte` varchar(400) NOT NULL,
+    `tag` int NOT NULL,
+    `date` DATE NOT NULL,
+    `valid` boolean,
+    PRIMARY KEY (`id_membre`)
+  );");
